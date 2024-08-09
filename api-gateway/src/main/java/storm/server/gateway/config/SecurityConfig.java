d6f5fe5bab4b9522bb75f31bed13fcfaac90c6ba
AnhHT <tamptit2016@gmail.com>
2024-08-10 05:41:18 +0700

    gate-way: test FilterChain. FluxSecurity to WebSecurity

--- api-gateway/src/main/java/storm/server/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/storm/server/gateway/config/SecurityConfig.java
@@ -1,20 +1,31 @@
 package storm.server.gateway.config;
 
-import io.netty.handler.codec.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.util.*;
+import java.util.concurrent.Flow;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebFluxSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
     private static final String GROUPS = "groups";
@@ -27,24 +38,24 @@ public class SecurityConfig {
         this.keycloakLogoutHandler = keycloakLogoutHandler;
     }
 
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(auth -> auth.anyExchange().authenticated())
-                .oauth2Login(withDefaults())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
-        return http.build();
-    }
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//        http.authorizeExchange(auth -> auth.anyExchange().authenticated())
+//                .oauth2Login(withDefaults())
+//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+//        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+//        return http.build();
+//    }
 
     @Bean
     public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers(new AntPathRequestMatcher("/customers*", HttpMethod.OPTIONS.name()))
-                .permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/customers*"))
-                .hasRole("user")
-                .requestMatchers(new AntPathRequestMatcher("/"))
-                .permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/customers*", HttpMethod.OPTIONS.name()))
+//                .permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/customers*"))
+//                .hasRole("user")
+//                .requestMatchers(new AntPathRequestMatcher("/"))
+//                .permitAll()
                 .anyRequest()
                 .authenticated());
         http.oauth2ResourceServer((oauth2) -> oauth2
@@ -52,5 +63,57 @@ public class SecurityConfig {
         http.oauth2Login(Customizer.withDefaults())
                 .logout(logout -> logout.addLogoutHandler(keycloakLogoutHandler).logoutSuccessUrl("/"));
         return http.build();
+    }
+
+//    @Bean
+//    public SecurityFilterChain basicFilterChain (HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(auth -> auth
+//                .requestMatchers(new AntPathRequestMatcher("/customers*", HttpMethod.OPTIONS.name()))
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated())
+//                .httpBasic(withDefaults());
+//        return http.build();
+//    }
+
+    @Bean
+    Collection<GrantedAuthority> generateAuthoritiesFromClaim(Collection<Object> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role)).collect(
+                Collectors.toList());
+    }
+
+    @Bean
+    public GrantedAuthoritiesMapper userAuthoritiesMapperForKeycloak() {
+        return authorities -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<GrantedAuthority>();
+            var authority = authorities.iterator().next();
+            boolean isOidc = authority instanceof OidcUserAuthority;
+
+            if (isOidc) {
+                var oidcUserAuthority = (OidcUserAuthority) authority;
+                var userInfo = oidcUserAuthority.getUserInfo();
+
+                // Tokens can be configured to return roles under
+                // Groups or REALM ACCESS hence have to check both
+                if (userInfo.hasClaim(REALM_ACCESS_CLAIM)) {
+                    Map<String, Object> realmAccess = userInfo.getClaimAsMap(REALM_ACCESS_CLAIM);
+                    var roles = realmAccess.get(ROLES_CLAIM);
+                    mappedAuthorities.addAll(generateAuthoritiesFromClaim((Collection<Object>) roles));
+                } else if (userInfo.hasClaim(GROUPS)) {
+                    Collection<Object> roles =  userInfo.getClaim(GROUPS);
+                    mappedAuthorities.addAll(generateAuthoritiesFromClaim(roles));
+                }
+            } else {
+                var oauth2UserAuthority = (OAuth2UserAuthority) authority;
+                Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
+
+                if (userAttributes.containsKey(REALM_ACCESS_CLAIM)) {
+                    Map<String, Object> realmAccess = (Map<String, Object>) userAttributes.get(REALM_ACCESS_CLAIM);
+                    Collection<Object> roles = (Collection<Object>) realmAccess.get(ROLES_CLAIM);
+                    mappedAuthorities.addAll(generateAuthoritiesFromClaim(roles));
+                }
+            }
+            return mappedAuthorities;
+        };
     }
 }
